e3371c5efc628fb6c472eb9d6b6e5c31240c4f68
Timothy Pham <timothy.pham.tap@gmail.com>
2024-01-27 20:06:23 -0800

    fixed index at root website

--- Website/base/urls.py
+++ Website/base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
+    path("", views.index, name="index_root"),
     path("index/", views.index, name="index"), 
     path("about/", views.about, name="about"), #about was swapped with home - for some reason this breaks when views.about and views.home are not switched
     path("savings/", views.savings, name="savings"),
