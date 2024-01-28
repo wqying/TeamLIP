fe4022c1e829909ed48288ab49acf31705f5f6ae
Timothy Pham <timothy.pham.tap@gmail.com>
2024-01-27 20:00:49 -0800

    heck it we ball

--- Website/base/urls.py
+++ Website/base/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    path("", views.home, name="home"), 
+    path("index/", views.index, name="index"), 
     path("about/", views.about, name="about"), #about was swapped with home - for some reason this breaks when views.about and views.home are not switched
     path("savings/", views.savings, name="savings"),
     path("debt/", views.debt, name="debt"),
-    path("index/", views.index, name="index"),
+    #path("index/", views.index, name="index"),
     path("investing/", views.investing, name="investing"), #may need to edit to match
 ]
