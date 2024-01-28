4f71ac151e7c4e0ad824081d948aa329ded92ed8
wqying <qywong@uci.edu>
2024-01-27 20:04:02 -0800

    Revert "fullversion"
    This reverts commit deaa3e03a9674fe48da3a0e41bbbd67533bde505.

---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+	
+	public void quitGame()
+	{
+		Application.Quit();
+	}
+	
+	void Update()
+	{
+		if (Input.GetKey("escape"))
+		{
+			Application.Quit();
+		}
+	}
+	
+
+}
