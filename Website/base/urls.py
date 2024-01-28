f4315e56d6f4f971cbacad1df31dcdfed9734fbe
Timothy Pham <timothy.pham.tap@gmail.com>
2024-01-28 11:33:55 -0800

    home html fix

--- Website/base/urls.py
+++ Website/base/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    path("", views.home, name="home"), 
+    path("", views.index, name="root"),
+    path("index/", views.index, name="index"), 
     path("about/", views.about, name="about"), #about was swapped with home - for some reason this breaks when views.about and views.home are not switched
     path("savings/", views.savings, name="savings"),
     path("debt/", views.debt, name="debt"),
